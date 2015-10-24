54e5acdeb9fd57c035f3ef1efa8eb22d3ebf318a
antonstefansson <antonstefansson@gmail.com>
2015-10-24 14:14:55 +0000

    added tests

--- src/test/java/is/hr/ticTacToe/ticTacToeTest.java
+++ src/test/java/is/hr/ticTacToe/ticTacToeTest.java
@@ -5,7 +5,34 @@ import org.junit.Test;
 
 public class ticTacToeTest
 {
-	public static void main(String args[]){
+	public static void main(String args[])
+	{
 		org.junit.runner.JUnitCore.main("is.hr.ticTacToe.ticTacToeTest");
+	}
+
+	@Test
+	public void testLegalInput()
+	{
+		try
+		{
+			assertEquals(0, ticTacToe.game(10));
+		}
+		catch(Exception e)
+		{
+			assertEquals("Illegal input", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testIsOccupied()
+	{
+		try
+		{
+			assertEquals(0, ticTacToe.game(3, testNumbers));
+		}
+		catch(Exception e)
+		{
+			assertEquals("Tile is occupied please pick a different tile", e.getMessage());
+		}
 	}
 }
