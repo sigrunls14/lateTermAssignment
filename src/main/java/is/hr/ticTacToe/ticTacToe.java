38dae7b78a999603b24aa1c4756195b26810bf65
Droken <valgeir14@ru.is>
2015-10-24 15:04:57 +0000

    added setUpBoard

--- src/main/java/is/hr/ticTacToe/ticTacToe.java
+++ src/main/java/is/hr/ticTacToe/ticTacToe.java
@@ -2,5 +2,27 @@ package is.hr.ticTacToe;
 
 public class ticTacToe
 {
-	
+	private char [][] board;
+	private char currentPlayer;
+
+	public ticTacToe()
+	{
+		board = new char [3][3];
+		currentPlayer = 'x';
+		setUpBoard();
+	}	
 }
+
+public void setUpBoard()
+{
+	// looping through X-axis
+	for(int i = 0; i < 3; i++)
+	{
+		// Looping through Y-axis
+		for(int j = 0; j < 3; j++)
+		{
+			board[i][j] = '-';
+		}
+	}
+}
+
