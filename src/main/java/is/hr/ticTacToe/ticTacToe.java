d2945b6ccb9cbff31ed634a3badbb6c58b218334
Droken <valgeir14@ru.is>
2015-10-29 14:31:45 +0000

    fixed code for build

--- src/main/java/is/hr/ticTacToe/ticTacToe.java
+++ src/main/java/is/hr/ticTacToe/ticTacToe.java
@@ -12,96 +12,113 @@ public class ticTacToe
 		currentPlayer = 'x';
 		setUpBoard();
 	}	
-}
 
-public void setUpBoard()
-{
-	// looping through X-axis
-	for(int i = 0; i < 3; i++)
+
+	public void setUpBoard()
 	{
-		// Looping through Y-axis
-		for(int j = 0; j < 3; j++)
+		// looping through X-axis
+		for(int i = 0; i < 3; i++)
 		{
-			board[i][j] = '-';
+			// Looping through Y-axis
+			for(int j = 0; j < 3; j++)
+			{
+				board[i][j] = '-';
+			}
 		}
 	}
-}
 
-public void input()
-{
-	char move;
-	Scanner input = new Scanner(System.input)
-	move = input.nextLine();
-	if(legalInput(move) == true)
+	/*public void input()
 	{
-		switch(Integer.parseInt(move))
+		char move;
+		Scanner input = new Scanner(System.input)
+		move = input.nextLine();
+		if(legalInput(move) == true)
 		{
-			case 1: board[0][0] = currentPlayer;
-					break;
-			case 2: board[0][1] = currentPlayer;
-					break;
-			case 3: board[0][2] = currentPlayer;
-					break;
-			case 4: board[1][0] = currentPlayer;
-					break;
-			case 5: board[1][1] = currentPlayer;
-					break;
-			case 6: board[1][2] = currentPlayer;
-					break;
-			case 7: board[2][0] = currentPlayer;
-					break;
-			case 8: board[2][1] = currentPlayer;
-					break;
-			case 9: board[2][2] = currentPlayer;
-					break;
+			switch(Integer.parseInt(move))
+			{
+				case 1: board[0][0] = currentPlayer;
+						break;
+				case 2: board[0][1] = currentPlayer;
+						break;
+				case 3: board[0][2] = currentPlayer;
+						break;
+				case 4: board[1][0] = currentPlayer;
+						break;
+				case 5: board[1][1] = currentPlayer;
+						break;
+				case 6: board[1][2] = currentPlayer;
+						break;
+				case 7: board[2][0] = currentPlayer;
+						break;
+				case 8: board[2][1] = currentPlayer;
+						break;
+				case 9: board[2][2] = currentPlayer;
+						break;
+			}
+		}
+		else
+		{
+			throw new RuntimeException("Illegal input");
 		}
 	}
-	else
-	{
-		throw new RuntimeException("Illegal input");
-	}
-}
 
-public boolean legalInput(char move)
-{
-	if((Integer.parseInt(move) > 0) && (Integer.parseInt(move) < 10))
+	public boolean legalInput(char move)
 	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-private char checkRows(char [][] board)
-{
-	char checkWinner = '-';
-
-	for(int i = 0; i < 3; i++)
-	{
-		//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
-		if((board[i][0] != '-') && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]))
+		if((Integer.parseInt(move) > 0) && (Integer.parseInt(move) < 10))
 		{
-			checkWinner = board[i][0];
+			return true;
 		}
-	}	
-
-	return checkWinner;
-}
-
-private char checkColumns(char [][] board)
-{
-	char checkWinner = '-';
-
-	for(int i = 0; i < 3; i++)
-	{
-		//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
-		if((board[0][i] != '-') && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i]))
+		else
 		{
-			checkWinner = board[0][i];
+			return false;
 		}
-	}	
+	}*/
 
-	return checkWinner;
+	public static char checkRows(char [][] board)
+	{
+		char checkWinner = '-';
+
+		for(int i = 0; i < 3; i++)
+		{
+			//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
+			if((board[i][0] != '-') && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]))
+			{
+				checkWinner = board[i][0];
+			}
+		}	
+
+		return checkWinner;
+	}
+
+	public static char checkColumns(char [][] board)
+	{
+		char checkWinner = '-';
+
+		for(int i = 0; i < 3; i++)
+		{
+			//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
+			if((board[0][i] != '-') && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i]))
+			{
+				checkWinner = board[0][i];
+			}
+		}	
+
+		return checkWinner;
+	}
+
+	public static char checkDiagonally(char [][] board)
+	{
+		char checkWinner = '-';
+
+		if((board[0][0] != '-') && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))
+		{
+			checkWinner = board[0][0];
+		}
+		else if ( (board[0][2] != '-') && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) 
+		{
+			checkWinner = board[0][2];
+		}
+
+		return checkWinner;
+	}
 }
