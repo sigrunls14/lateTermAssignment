4ac6796a48ee7370d1921d6f3c0a46c977ec6a7e
Droken <valgeir14@ru.is>
2015-11-02 15:01:52 +0000

    moved some functions between tictactoe and board

--- src/main/java/is/hr/ticTacToe/ticTacToe.java
+++ src/main/java/is/hr/ticTacToe/ticTacToe.java
@@ -50,60 +50,9 @@ public class ticTacToe
 		}
 	}
 
-	//Checks the rows on the board and returns either - if no winner, X if X won a row and O if O won a row
-	static public char checkRows(char [][] board)
-	{
-		char checkWinner = '-';
-
-		for(int i = 0; i < 3; i++)
-		{
-			//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
-			if((board[i][0] != '-') && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]))
-			{
-				checkWinner = board[i][0];
-			}
-		}	
-
-		return checkWinner;
-	}
-
-	// Same as checkRows function, returns either -, X or O
-	static public char checkColumns(char [][] board)
-	{
-		char checkWinner = '-';
-
-		for(int i = 0; i < 3; i++)
-		{
-			//Checks if the first column in the row is either X or O, then compares the row to check if either of them won
-			if((board[0][i] != '-') && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i]))
-			{
-				checkWinner = board[0][i];
-			}
-		}	
-
-		return checkWinner;
-	}
-
-	// Same as checkColumns and checkRows function, returns either -, X or O
-	static public char checkDiagonally(char [][] board)
-	{
-		char checkWinner = '-';
-
-		if((board[0][0] != '-') && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))
-		{
-			checkWinner = board[0][0];
-		}
-		else if ( (board[0][2] != '-') && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) 
-		{
-			checkWinner = board[0][2];
-		}
-
-		return checkWinner;
-	}
-
 	public boolean checkWinner()
 	{
-		if((checkRows() || checkColumns() || iacheckDiagonally()) != '-')
+		if(( Board.checkRows() || Board.checkColumns(); || Board.checkDiagonally() ) != '-')
 		{
 			return true;
 		}
